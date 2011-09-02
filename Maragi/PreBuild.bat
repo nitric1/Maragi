a48b567bff0616a5c0e078d1cc93b5b9e2567978
Nitric <mail@nitric.me>
2011-09-02 13:03:06 +0000

    git-svn-id: http://anonsvn.hno3.pe.kr/Maragi@17 4001e694-9514-468b-8fa2-1501e3eb7756

--- Maragi/PreBuild.bat
+++ Maragi/PreBuild.bat
@@ -1,4 +1,16 @@
-rem $Id$
 @echo off
+
+rem $Id$
+
+if "%1" == "" goto end
+if not exist %1\Tokens.txt goto create
+goto process
+
+:create
+echo > %1\Tokens.txt
+
+:process
 echo Merging Tokens.h.in and Tokens.txt into Tokens.h...
-perl %1 %2 %3 > %4
+perl %1\TextReplacer.pl %1\Tokens.h.in %1\Tokens.txt > %1\Tokens.h
+
+:end
