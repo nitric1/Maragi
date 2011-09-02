19c99fe4a6147b3b684ea9c8c578a3e68fc0ad0f
Nitric <mail@nitric.me>
2011-09-02 12:18:30 +0000

    Token Work
    git-svn-id: http://anonsvn.hno3.pe.kr/Maragi@12 4001e694-9514-468b-8fa2-1501e3eb7756

---
+++ Maragi/Tokens.cpp
@@ -0,0 +1,14 @@
+// $Id$
+
+#include "Defaults.h"
+
+#include "Tokens.h"
+
+namespace Maragi
+{
+	namespace AppTokens
+	{
+		const char *consumerKey = MARAGI_CONSUMER_KEY;
+		const char *consumerSecret = MARAGI_CONSUMER_SECRET;
+	}
+}
